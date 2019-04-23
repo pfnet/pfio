26182fef19cac16097b9a5cb841f3fabaef7d8c7
Tianqi Xu <tianqi@preferred.jp>
2019-04-23 20:07:11 +0900

    Move to pfnci
    This commit removes the old circleci to use pfnci.

---
+++ .pfnci/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+set -eux
+
+gcloud auth configure-docker
+
+docker run --interactive --rm \
+       --volume "$(pwd):/repo/" --workdir /repo/ \
+       tianqixu/chainerio \
+       sh -ex << EOD
+pip3 install --user -e .
+py.test tests
+flake8 chainerio
+flake8 tests
+autopep8 -r chainerio tests --diff | tee check_autopep8
+test ! -s check_autopep8
+EOD
