7389c8c670fd01c34a6f34810aa08aa5f3f2ff47
Tianqi Xu <tianqi@preferred.jp>
2019-05-07 17:42:49 +0900

    Add Multiple config tests
    This commit adds multi-config test to test with different python
    versions.
    This commit tests the latest minor versions of all python > 3.5.

--- .pfnci/test.sh
+++ .pfnci/test.sh
@@ -5,10 +5,13 @@ gcloud auth configure-docker
 
 docker run --interactive --rm \
        --volume "$(pwd):/repo/" --workdir /repo/ \
-       tianqixu/chainerio \
-       sh -ex << EOD
-pip3 install --user -e .
-py.test tests -s -v
+       tianqixu/chainerio:ubuntu \
+       bash -ex << EOD
+source /root/.bashrc
+pyenv global ${PYTHON}
+pip install --user -e .[test]
+export PATH=/root/.local/bin:$PATH
+pytest tests -s -v
 flake8 chainerio
 flake8 tests
 autopep8 -r chainerio tests --diff | tee check_autopep8
