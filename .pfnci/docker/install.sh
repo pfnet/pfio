99779a64d9d24a14015ae901f82c9b711946d85b
Tianqi Xu <tianqi@preferred.jp>
2019-05-08 15:10:46 +0900

    Add tox
    This commit changes the CI setting to use tox to test different python
    versions that are supported by ChainerIO. The latest patched version of
    each minor version are tested.
    Currently, we test the following:
    3.5.7
    3.6.8
    3.7.2

---
+++ .pfnci/docker/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function install_py()
+{
+    PYTHON_VERSION=$1
+    PYENV_ROOT=$2
+    CFLAGS=-I/usr/include/openssl
+    LDFLAGS=-L/usr/lib pyenv install $PYTHON_VERSION
+    pyenv shell $PYTHON_VERSION
+    pyenv global $PYTHON_VERSION
+}
+
+python_versions=('3.5.7' '3.6.8' '3.7.2')
+PYENV_ROOT=/root/.pyenv
+
+rm -rf $PYENV_ROOT
+git clone git://github.com/pyenv/pyenv.git $PYENV_ROOT
+export PATH=$PYENV_ROOT/bin:$PATH
+export MAKE_OPTS=-j16
+$PYENV_ROOT/plugins/python-build/install.sh
+eval "$(pyenv init -)"
+echo "export PATH=$PYENV_ROOT/bin:$PATH" >> /root/.bashrc
+echo "eval '$(pyenv init -)'" >> /root/.bashrc
+# remove the return on non-interactive bash
+sed -i '6d' /root/.bashrc
+
+for version in "${python_versions[@]}"
+do
+    install_py $version $PYENV_ROOT
+done
+
+# install tox in the newest python
+pip install tox
--- .pfnci/test.sh
+++ .pfnci/test.sh
@@ -5,12 +5,9 @@ gcloud auth configure-docker
 
 docker run --interactive --rm \
        --volume "$(pwd):/repo/" --workdir /repo/ \
-       tianqixu/chainerio \
-       sh -ex << EOD
-pip3 install --user -e .
-py.test tests -s -v
-flake8 chainerio
-flake8 tests
-autopep8 -r chainerio tests --diff | tee check_autopep8
-test ! -s check_autopep8
+       chainer/chainerio:latest \
+       bash << EOD
+source /root/.bashrc
+pyenv global 3.5.7 3.6.8 3.7.2
+tox
 EOD
