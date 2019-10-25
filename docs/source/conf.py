baa947046bc0450012e1d2e16e2478b2738b4b9e
belldandyxtq <tianqi@preferred.jp>
2019-10-25 17:34:35 +0900

    release version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Preferred Networks, Inc.'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.6'
 
 
 # -- General configuration ---------------------------------------------------
