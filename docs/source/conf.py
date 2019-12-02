80c1a737d54a7e7cfec311ef0a51adec68e69d24
belldandyxtq <tianqi@preferred.jp>
2019-12-02 18:14:01 +0900

    dump up to 0.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Preferred Networks, Inc.'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.6'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
