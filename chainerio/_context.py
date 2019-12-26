c545965de6b422252319349f632c9b8c9eb09eaa
tianqi xu <tianqi@preferred.jp>
2019-12-26 16:51:17 +0900

    Add global handler cache and refactoring

--- chainerio/_context.py
+++ chainerio/_context.py
@@ -18,40 +18,35 @@ class FileSystemDriverList(object):
         self.pattern_list = {"hdfs": self.hdfs_pattern,
                              "posix": self.posix_pattern, }
 
-    def _determine_fs_type(self, path: str) -> Tuple[str, str, bool]:
-        if None is not path:
-            for fs_type, pattern in self.pattern_list.items():
-                ret = pattern.match(path)
-                if ret:
-                    return (fs_type, ret.groupdict()["path"], True)
+    def format_path(self, path: str) -> Tuple[str, str, bool]:
+        if path in self.scheme_list:
+            # when the path is just a scheme
+            return (path, "", True)
+
+        for fs_type, pattern in self.pattern_list.items():
+            ret = pattern.match(path)
+            if ret:
+                return (fs_type, ret.groupdict()["path"], True)
 
         return ("posix", path, False)
 
-    def format_path(self, fs: IO, path: str) -> Tuple[str, bool]:
-        fs_type = fs.type
-        if fs_type in self.pattern_list.keys():
-            pattern = self.pattern_list[fs_type]
-            ret = pattern.match(path)
-            if ret:
-                return (ret.groupdict()["path"], True)
-            else:
-                return (path, False)
+    def _create_handler_from_path(self, path: str,
+                                  fs_type: str = None) -> Tuple[IO, str, bool]:
+        if fs_type is None:
+            (fs_type, actual_path, is_URI) = self.format_path(path)
         else:
-            return (path, False)
-
-    def get_handler_from_path(self, path: str) -> Tuple[IO, str, bool]:
-        (fs_type, actual_path, is_URI) = self._determine_fs_type(path)
+            actual_path = path
+            is_URI = False
 
         handler = create_fs_handler(fs_type)
         return (handler, actual_path, is_URI)
 
-    def get_handler_for_root(self,
-                             uri_or_handler_name: str) -> Tuple[IO, str, bool]:
+    def get_handler(self, uri_or_handler_name: str) -> Tuple[IO, str, bool]:
         if uri_or_handler_name in self.pattern_list.keys():
             return (create_fs_handler(uri_or_handler_name), "", False)
         else:
-            (new_handler, actual_path, is_URI) = self.get_handler_from_path(
-                uri_or_handler_name)
+            (new_handler, actual_path, is_URI) = \
+                self._create_handler_from_path(uri_or_handler_name)
             new_handler.root = actual_path
             return (new_handler, actual_path, is_URI)
 
@@ -62,55 +57,52 @@ class FileSystemDriverList(object):
 class DefaultContext(object):
     def __init__(self):
         self._fs_handler_list = FileSystemDriverList()
-        self._root = ""
 
         self._default_context = \
-            self._fs_handler_list.get_handler_for_root("posix")[0]
+            self._fs_handler_list.get_handler("posix")[0]
+
+        self._global_handler_cache = {}
 
     def set_root(self, uri_or_handler: Union[str, IO]) -> None:
-        # TODO(check) if root is directory
         if isinstance(uri_or_handler, IO):
             handler = uri_or_handler
-            self._root = ""
         else:
-            (handler, self._root, is_URI) = \
-                self.get_handler_by_name(uri_or_handler)
-        assert handler is not None
+            handler, path = self._get_handler_no_root(uri_or_handler)
+            handler.root = path
 
-        if self._root:
-            if not handler.isdir(self._root):
-                raise RuntimeError("the URI does not point to a directory")
+            if handler.root:
+                if not handler.isdir(handler.root):
+                    raise RuntimeError("""the URI '{}' does not
+                        point to a directory""".format(handler.root))
 
         self._default_context = handler
 
-    def get_handler(self, path: str = "") -> Tuple[IO, str]:
-        (handler, formatted_path,
-         is_URI) = self._fs_handler_list.get_handler_from_path(path)
+    def _get_handler_no_root(self, path: str = "") -> Tuple[IO, str]:
+        (fs_type, path, is_URI) = self._fs_handler_list.format_path(path)
 
         if not is_URI:
-            actual_path = os.path.join(self._root, formatted_path)
-            return (self._default_context, actual_path)
-        else:
-            return (handler, formatted_path)
-
-    def open_as_container(self, path: str) -> Container:
-        (handler, formatted_path,
-         is_URI) = self._fs_handler_list.get_handler_from_path(path)
-
-        if not is_URI:
-            actual_path = os.path.join(self._root, formatted_path)
             handler = self._default_context
         else:
-            actual_path = formatted_path
+            if fs_type in self._global_handler_cache:
+                # get handler from cache
+                handler = self._global_handler_cache[fs_type]
+            else:
+                # create a new handler
+                handler = create_fs_handler(fs_type)
+                self._global_handler_cache[fs_type] = handler
+        return (handler, path)
 
-        self._root = ""
+    def get_handler(self, path: str = "") -> Tuple[IO, str]:
+        handler, path = self._get_handler_no_root(path)
+        actual_path = os.path.join(handler.root, path)
+        return (handler, actual_path)
+
+    def open_as_container(self, path: str) -> Container:
+        (handler, actual_path) = self.get_handler(path)
         return handler.open_as_container(actual_path)
 
-    def get_handler_by_name(self, path: str) -> Tuple[IO, str, bool]:
-        return self._fs_handler_list.get_handler_for_root(path)
-
     def get_root_dir(self) -> str:
-        return self._root
+        return self._default_context.root
 
     def is_supported_scheme(self, scheme: str) -> bool:
         return self._fs_handler_list.is_supported_scheme(scheme)
