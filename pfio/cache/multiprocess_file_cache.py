16572d9817c551ba3efcef29439123618bcca72d
Daichi SUZUO <belltailjp@gmail.com>
2020-08-16 19:00:30 +0900

    Same interface and behavior as the existing FileCache

--- pfio/cache/multiprocess_file_cache.py
+++ pfio/cache/multiprocess_file_cache.py
@@ -12,13 +12,40 @@ import pickle
 
 
 class MultiprocessFileCache(cache.Cache):
+    '''Multiprocess-safe cache system with local filesystem
+
+    Stores cache data in local temporary files, created in
+    ``~/.pfio/cache`` by default. Cache data is
+    automatically deleted after the object is collected. When this
+    object is not correctly closed, (e.g., the process killed by
+    SIGTERM), the cache remains after the death of process.
+
+    This class supports handling a cache from multiple processes.
+    A MultiprocessFileCache object can be handed over to another process
+    through the pickle. Calling ``get`` and ``put`` in each process will
+    look into the same cache files which are created by the initializer.
+    The temporary cache files will persist until the MultiprocessFileCache
+    object is destroyed in the original process it is created.
+    This means that even after the worker processes are destroyed,
+    the MultiprocessFileCache object can be passed to another processes,
+    with the cache files remain accessible.
+
+    Arguments:
+        length (int): Length of the cache array.
+
+        do_pickle (bool):
+            Do automatic pickle and unpickle inside the cache.
+
+        dir (str): The path to the directory to place cache data in
+            case home directory is not backed by fast storage device.
+
+    '''
 
     def __init__(self, length, do_pickle=False,
-                 dir=None, verbose=False,
-                 index_cache_file=None, data_cache_file=None,
-                 cleanup=True):
+                 dir=None, verbose=False):
         self.length = length
         self.do_pickle = do_pickle
+        self.verbose = verbose
         assert self.length > 0
 
         if dir is None:
@@ -27,19 +54,11 @@ class MultiprocessFileCache(cache.Cache):
             self.dir = dir
         os.makedirs(self.dir, exist_ok=True)
 
-        self.cleanup = cleanup
         self.closed = False
-        if not data_cache_file:
-            _, self.data_file = tempfile.mkstemp(dir=self.dir)
-        else:
-            self.data_file = data_cache_file
-
-        if not index_cache_file:
-            index_fd, self.index_file = tempfile.mkstemp(dir=self.dir)
-        else:
-            self.index_file = index_cache_file
-            flag = os.O_WRONLY | os.O_TRUNC | os.O_CREAT
-            index_fd = os.open(index_cache_file, flag)
+        self._frozen = False
+        self._master_pid = os.getpid()
+        _, self.data_file = tempfile.mkstemp(dir=self.dir)
+        index_fd, self.index_file = tempfile.mkstemp(dir=self.dir)
 
         try:
             fcntl.flock(index_fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
@@ -55,26 +74,24 @@ class MultiprocessFileCache(cache.Cache):
 
             # Clear data file
             os.close(os.open(self.data_file, os.O_WRONLY | os.O_TRUNC))
-
-            fcntl.flock(index_fd, fcntl.LOCK_UN)
         except OSError as ose:
             # Lock acquisition error -> No problem, since other worker
             # should be already working on it
             if ose.errno not in (errno.EACCESS, errno.EAGAIN):
                 raise
-
-    def get_offsets(self):
-        fd = os.open(self.index_file, os.O_RDONLY)
-        offsets = []
-        for i in range(self.length):
-            offset = self.buflen * i
-            buf = os.pread(fd, self.buflen, offset)
-            (o, l) = unpack('Qq', buf)
-            offsets.append(o)
-        return offsets
+        finally:
+            fcntl.flock(index_fd, fcntl.LOCK_UN)
 
     def __len__(self):
         return self.length
+
+    @property
+    def multiprocess_safe(self) -> bool:
+        return True
+
+    @property
+    def multithread_safe(self) -> bool:
+        return True
 
     def get(self, i):
         if self.closed:
@@ -105,6 +122,9 @@ class MultiprocessFileCache(cache.Cache):
         return data
 
     def put(self, i, data):
+        if self._frozen:
+            return
+
         try:
             if self.do_pickle:
                 data = pickle.dumps(data)
@@ -163,22 +183,70 @@ class MultiprocessFileCache(cache.Cache):
         try:
             unlink(fn)
         except FileNotFoundError:
-            # Other worker may have already deleted it
+            # Another worker may have already deleted it
             pass
 
     def close(self):
         if not self.closed:
-            self.closed = True
-            if self.cleanup:
+            if os.getpid() == self._master_pid:
                 self._deleter(self.data_file)
                 self._deleter(self.index_file)
+            self.closed = True
             self.data_file = None
             self.index_file = None
 
-    @property
-    def multiprocess_safe(self) -> bool:
-        return True
+    def preload(self, name):
+        '''Load the cache saved by ``preserve()``
 
-    @property
-    def multithread_safe(self) -> bool:
-        return True
+        After loading the files, no data can be added to the cache.
+        ``name`` is the prefix of the persistent files.
+
+        .. note:: This feature is experimental.
+
+        '''
+        if self._frozen:
+            return
+
+        # Overwrite the current cache by the specified cache file.
+        # This is needed to prevent the specified cache files are deleted when
+        # the cache object is destroyed.
+        ld_index_file = os.path.join(self.dir, '{}.cachei'.format(name))
+        ld_data_file = os.path.join(self.dir, '{}.cached'.format(name))
+        if any(not os.path.exists(p) for p in (ld_index_file, ld_data_file)):
+            raise ValueError('Specified cache "{}" not found in {}'
+                             .format(name, self.dir))
+
+        self._deleter(self.data_file)
+        self._deleter(self.index_file)
+        os.link(ld_index_file, self.index_file)
+        os.link(ld_data_file, self.data_file)
+        self._frozen = True
+
+    def preserve(self, name):
+        '''Preserve the cache as persistent files on the disk
+
+        Once the cache is preserved, cache files will not be removed
+        at cache close. To read data from preserved files, use
+        ``preload()`` method. After preservation, no data can be added
+        to the cache.  ``name`` is the prefix of the persistent
+        files.
+
+        .. note:: This feature is experimental.
+
+        '''
+
+        index_file = os.path.join(self.dir, '{}.cachei'.format(name))
+        data_file = os.path.join(self.dir, '{}.cached'.format(name))
+        fd = os.open(self.index_file, os.O_RDONLY)
+        try:
+            fcntl.flock(fd, fcntl.LOCK_EX)
+            os.link(self.index_file, index_file)
+            os.link(self.data_file, data_file)
+
+        except OSError as ose:
+            # Lock acquisition error -> No problem, since other worker
+            # should be already working on it
+            if ose.errno not in (errno.EACCESS, errno.EAGAIN):
+                raise
+        finally:
+            fcntl.flock(fd, fcntl.LOCK_UN)
