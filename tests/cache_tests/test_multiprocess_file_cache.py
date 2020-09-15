69596f50a9e07e928acf2dba13a3e4517638232d
Daichi SUZUO <belltailjp@gmail.com>
2020-09-15 16:25:37 +0900

    Multiprocess safety testing

--- tests/cache_tests/test_multiprocess_file_cache.py
+++ tests/cache_tests/test_multiprocess_file_cache.py
@@ -4,6 +4,8 @@ import os
 import pickle
 import tempfile
 
+import numpy as np
+
 import pytest
 
 
@@ -49,6 +51,41 @@ def test_cleanup_subprocess():
         cache.close()
 
         assert len(os.listdir(d)) == 0
+
+
+def test_multiprocess_consistency():
+    # Condition: 32k samples (8k*4bytes each) cached by 64 workers.
+    # Each sample is an array of repeated sample index.
+    # ie. k-th sample is np.array([k, k, k, ..., k], dtype=np.int32)
+    # 32 worker processes simultaneously create such data and insert them into
+    # a single cache, and we check if the data can be correctly recovered.
+    n_workers = 32
+    n_samples_per_worker = 1024
+    sample_size = 8192
+
+    def child(cache, worker_idx):
+        for i in range(n_samples_per_worker):
+            sample_idx = worker_idx * n_samples_per_worker + i
+            data = np.array([sample_idx] * sample_size, dtype=np.int32)
+            cache.put(sample_idx, data)
+
+    with tempfile.TemporaryDirectory() as d:
+        cache = MultiprocessFileCache(n_samples_per_worker * n_workers,
+                                      dir=d, do_pickle=True)
+
+        # Add tons of data into the cache in parallel
+        ps = [multiprocessing.Process(target=child, args=(cache, worker_idx))
+              for worker_idx in range(n_workers)]
+        for i, p in enumerate(ps):
+            p.start()
+        for i, p in enumerate(ps):
+            p.join()
+
+        # Get each sample from the cache and check the content
+        for sample_idx in range(n_workers * n_samples_per_worker):
+            data = cache.get(sample_idx)
+            expected = np.array([sample_idx] * sample_size, dtype=np.int32)
+            assert (data == expected).all()
 
 
 def test_preservation():
