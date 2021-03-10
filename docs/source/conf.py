62889052c92ceb277edebdcab18c4ce97d91b762
UENISHI Kota <kuenishi@users.noreply.github.com>
2021-03-10 14:11:53 +0900

    Bump version (#158)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Preferred Networks, Inc.'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.2.0'
 
 
 # -- General configuration ---------------------------------------------------
