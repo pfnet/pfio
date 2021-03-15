a6fa91a88dae56fcee7a0dea838daca6c1b179e0
UENISHI Kota <kuenishi@gmail.com>
2021-03-15 12:06:22 +0900

    No use of NFS for file cache

--- tests/cache_tests/test_file_cache.py
+++ tests/cache_tests/test_file_cache.py
@@ -1,6 +1,10 @@
 import tempfile
 
+import pytest
+
+import pfio
 from pfio.cache import FileCache, MultiprocessFileCache
+from pfio.testing import patch_subprocess
 
 
 def test_preservation_interoperability():
@@ -22,3 +26,24 @@ def test_preservation_interoperability():
         assert cache2.preload('preserved') is True
         for i in range(10):
             assert str(i) == cache2.get(i)
+
+
+@patch_subprocess(b'ext3/ext4')
+def test_no_nfs():
+    FileCache(dir='var', length=214)
+
+    MultiprocessFileCache(dir='var', length=234)
+
+
+@patch_subprocess(b'nfs')
+def test_nfs():
+    with pytest.raises(ValueError):
+        FileCache(dir='var', length=214)
+
+    with pytest.raises(ValueError):
+        MultiprocessFileCache(dir='var', length=214)
+
+    pfio.cache.file_cache._FORCE_LOCAL = False
+
+    FileCache(dir='var', length=214)
+    MultiprocessFileCache(dir='var', length=214)
