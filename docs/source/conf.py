4e378fddaee0cd9eb441fd53935987e57580f1ba
UENISHI Kota <kuenishi@gmail.com>
2021-03-15 20:12:20 +0900

    Bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Preferred Networks, Inc.'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.2.2'
+release = '1.3.0'
 
 
 # -- General configuration ---------------------------------------------------
