72aa901f40c35a7497a63a896da04bea72e3b3d0
UENISHI Kota <kuenishi@users.noreply.github.com>
2021-09-01 15:31:52 +0900

    Fix BufferedIOBase requires readinto() implemented (#193)

--- tests/v2_tests/test_s3.py
+++ tests/v2_tests/test_s3.py
@@ -30,6 +30,18 @@ def test_s3():
                 assert 'bar' == fp.read()
                 assert not fp.closed
 
+            with s3.open('foo.txt', 'rb') as fp:
+                assert b'b' == fp.read(1)
+                assert b'a' == fp.read(1)
+                assert b'r' == fp.read(1)
+                assert b'' == fp.read(1)
+                assert b'' == fp.read(1)
+                fp.seek(1)
+                assert b'a' == fp.read(1)
+                assert b'r' == fp.read(1)
+                assert b'' == fp.read(1)
+                assert not fp.closed
+
             assert ['foo.txt'] == list(s3.list())
             assert [] == list(s3.list('base'))
             assert [] == list(s3.list('base/'))
@@ -49,8 +61,13 @@ def test_s3():
             p.join()
             assert p.exitcode == 0
 
+
+@mock_s3
+def test_s3_mpu():
     # Test multipart upload
     bucket = 'test-mpu'
+    key = "it's me!deadbeef"
+    secret = "asedf;lkjdf;a'lksjd"
     with S3(bucket, create_bucket=True, mpu_chunksize=8*1024*1024,
             aws_access_key_id=key,
             aws_secret_access_key=secret) as s3:
