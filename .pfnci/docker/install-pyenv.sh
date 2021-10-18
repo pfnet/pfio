4c99375ef4323848a7aae4e27b0ff14f22c8b681
UENISHI Kota <kuenishi@gmail.com>
2021-10-18 20:34:19 +0900

    Update pyenv to 2.1.0

--- .pfnci/docker/install-pyenv.sh
+++ .pfnci/docker/install-pyenv.sh
@@ -4,4 +4,5 @@ export PATH=$PYENV_ROOT/bin:$PATH
 $PYENV_ROOT/plugins/python-build/install.sh
 
 echo "export PATH=$PYENV_ROOT/bin:$PATH" >> /root/.bash_docker
+echo "eval '$(pyenv init --path)'" >> /root/.bash_docker
 echo "eval '$(pyenv init -)'" >> /root/.bash_docker
--- .pfnci/test.sh
+++ .pfnci/test.sh
@@ -2,12 +2,12 @@
 set -eux
 
 source /root/.bash_docker
-pyenv global 3.6.13 3.7.10 3.8.9 3.9.4
+pyenv global 3.6.15 3.7.12 3.8.12 3.9.7
 tox && :
 tox_status=$?
 
 # test doc, needs python >= 3.6
-pyenv global 3.9.4
+pyenv global 3.9.7
 pip install .[doc]
 cd docs && make html && :
 sphinx_status=$?
