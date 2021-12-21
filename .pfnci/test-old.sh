389f9dbaa039437ee1fe2dea12587e46f1aa2f64
UENISHI Kota <kuenishi@gmail.com>
2021-12-21 13:28:32 +0900

    Update run test

--- .pfnci/test-old.sh
+++ .pfnci/test-old.sh
@@ -2,5 +2,7 @@
 set -eux
 
 source /root/.bash_docker
-pyenv global 3.6.15 3.7.12 #3.8.12 3.9.7 3.10.0
+pyenv global 3.6.15 #3.7.12 3.8.12 3.9.7 3.10.0
+pip install tox
+pip install -e .[test]
 tox -e py36
