14d1dba861fb04d1325d340b4c3ec42082b8ea33
Daichi SUZUO <belltailjp@gmail.com>
2021-12-22 17:42:01 +0900

    Seek test

--- tests/v2_tests/test_s3.py
+++ tests/v2_tests/test_s3.py
@@ -1,9 +1,10 @@
+import os
 import multiprocessing as mp
 
 import pytest
 from moto import mock_s3
 
-from pfio.v2 import S3, from_url
+from pfio.v2 import S3, from_url, open_url
 from pfio.v2.fs import ForkedError
 
 
@@ -158,3 +159,60 @@ def test_s3_recursive():
             assert 3 == len(abspaths)
             for p in abspaths:
                 assert p.startswith('base/')
+
+
+@mock_s3
+def test_s3_seek():
+    bucket = "test-dummy-bucket"
+    key = "it's me!deadbeef"
+    secret = "asedf;lkjdf;a'lksjd"
+    with S3(bucket, create_bucket=True):
+        with from_url('s3://test-dummy-bucket/base',
+                      aws_access_key_id=key,
+                      aws_secret_access_key=secret) as s3:
+
+            touch(s3, 'foo.data', '1234567890')
+
+        with open_url('s3://test-dummy-bucket/base/foo.data',
+                      aws_access_key_id=key,
+                      aws_secret_access_key=secret) as f:
+            # Seek absolute position (position from start)
+            f.seek(0, os.SEEK_SET)
+            assert f.tell() == 0
+
+            f.seek(5, os.SEEK_SET)
+            assert f.tell() == 5
+
+            f.seek(15, os.SEEK_SET)
+            assert f.tell() == 10, "Position exceeding the stream size should be truncated"
+
+            f.seek(-4, os.SEEK_SET)
+            assert f.tell() == 6
+
+            # Relative seek
+            f.seek(0, os.SEEK_SET)  # back to start
+
+            f.seek(5, os.SEEK_CUR)
+            assert f.tell() == 5
+
+            f.seek(3, os.SEEK_CUR)
+            assert f.tell() == 8
+
+            f.seek(4, os.SEEK_CUR)
+            assert f.tell() == 10, "Position exceeding the stream size should be truncated"
+
+            f.seek(-1, os.SEEK_CUR)
+            assert f.tell() == 9
+
+            # Seek from tail
+            f.seek(0, os.SEEK_END)
+            assert f.tell() == 10
+
+            f.seek(-2, os.SEEK_END)
+            assert f.tell() == 8
+
+            f.seek(-12, os.SEEK_END)
+            assert f.tell() == 0, "Position exceeding the stream size should be truncated"
+
+            f.seek(2, os.SEEK_END)
+            assert f.tell() == 10, "Position exceeding the stream size should be truncated"
