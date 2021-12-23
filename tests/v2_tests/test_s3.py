ddb148d2aaf57d57e3f1d846ea55e5d81271878f
Daichi SUZUO <belltailjp@users.noreply.github.com>
2021-12-23 10:00:28 +0900

    [Ready] Fix `_ObjectReader.seek(0, os.SEEK_END)` case (#242)
    * Fix _ObjectReader.seek(0, os.SEEK_END) case
    
    * Seek test
    
    * Make _ObjectReader.seek behavior consitent to normal file-like objects
    
    * fix ci

--- tests/v2_tests/test_s3.py
+++ tests/v2_tests/test_s3.py
@@ -1,9 +1,11 @@
 import multiprocessing as mp
+import os
+import tempfile
 
 import pytest
 from moto import mock_s3
 
-from pfio.v2 import S3, from_url
+from pfio.v2 import S3, from_url, open_url
 from pfio.v2.fs import ForkedError
 
 
@@ -158,3 +160,70 @@ def test_s3_recursive():
             assert 3 == len(abspaths)
             for p in abspaths:
                 assert p.startswith('base/')
+
+
+def _seek_check(f):
+    # Seek by absolute position
+    ###########################
+    assert f.seek(0, os.SEEK_SET) == 0 and f.read() == b'0123456789'
+    assert f.seek(5, os.SEEK_SET) == 5 and f.read() == b'56789'
+    assert f.seek(15, os.SEEK_SET) == 15 and f.read() == b''
+
+    with pytest.raises(OSError) as err:
+        f.seek(-1, os.SEEK_SET)
+    assert err.value.errno == 22
+    assert f.tell() == 15, "the position should be kept after an error"
+
+    # Relative seek
+    ###############
+    f.seek(0, os.SEEK_SET)  # back to the start
+    assert f.seek(5, os.SEEK_CUR) == 5
+    assert f.seek(3, os.SEEK_CUR) == 8
+    assert f.seek(4, os.SEEK_CUR) == 12
+    assert f.seek(-1, os.SEEK_CUR) == 11
+
+    f.seek(0, os.SEEK_SET)
+    with pytest.raises(OSError) as err:
+        f.seek(-1, os.SEEK_CUR)
+    assert err.value.errno == 22
+    assert f.tell() == 0, "the position should be kept after an error"
+
+    # Seek from the tail
+    ####################
+    assert f.seek(0, os.SEEK_END) == 10
+    assert f.seek(-2, os.SEEK_END) == 8
+    assert f.seek(2, os.SEEK_END) == 12
+
+    with pytest.raises(OSError) as err:
+        f.seek(-11, os.SEEK_END) == 0
+    assert err.value.errno == 22
+    assert f.tell() == 12, "the position should be kept after an error"
+
+
+@mock_s3
+def test_s3_seek():
+    bucket = "test-dummy-bucket"
+    key = "it's me!deadbeef"
+    secret = "asedf;lkjdf;a'lksjd"
+    with S3(bucket, create_bucket=True):
+        with from_url('s3://test-dummy-bucket/base',
+                      aws_access_key_id=key,
+                      aws_secret_access_key=secret) as s3:
+
+            # Make a 10-bytes test data
+            touch(s3, 'foo.data', '0123456789')
+
+        with open_url('s3://test-dummy-bucket/base/foo.data', 'rb',
+                      aws_access_key_id=key,
+                      aws_secret_access_key=secret) as f:
+            _seek_check(f)
+
+    # Make sure the seek behavior is same as normal file-like objects.
+    with tempfile.NamedTemporaryFile() as tmpf:
+        # Make the same 10-bytes test data on local filesystem
+        with open(tmpf.name, 'wb') as f:
+            f.write(b'0123456789')
+
+        # Open and check its seek behavior is identical
+        with open(tmpf.name, 'rb') as f:
+            _seek_check(f)
