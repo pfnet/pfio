5fe136dee13075e0543f5d38a4e2ab1b149b0599
UENISHI Kota <kuenishi@gmail.com>
2022-01-04 16:56:24 +0900

    Fix isort

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,13 +8,13 @@
 
 # -- Path setup --------------------------------------------------------------
 
+import os
+import sys
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 from datetime import datetime
-import os
-import sys
 
 sys.path.insert(0, os.path.abspath('../../'))
 # sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
