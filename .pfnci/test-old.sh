2ea62c44f517a8d1635cdfbf5e4d3cbec7fce15f
UENISHI Kota <kuenishi@gmail.com>
2022-02-02 11:50:52 +0900

    Drop Python 3.6

--- .pfnci/test-old.sh
+++ .pfnci/test-old.sh
@@ -2,8 +2,8 @@
 set -eux
 
 source /root/.bash_docker
-pyenv global 3.6.15 #3.7.12 3.8.12 3.9.7 3.10.0
+pyenv global 3.7.12 #3.8.12 3.9.7 3.10.0
 python -m pip install --upgrade pip
 pip install tox
 pip install -e .[test]
-tox -e py36
+tox -e py37
