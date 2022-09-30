28752142701e8dac2170b88b51070b1a9b82f6d2
UENISHI Kota <kuenishi@gmail.com>
2022-09-30 20:53:40 +0900

    Configure Docker for new registry

--- .pfnci/script-old.sh
+++ .pfnci/script-old.sh
@@ -4,8 +4,8 @@ set -eux
 
 cd "$(dirname "${BASH_SOURCE}")"/..
 
-gcloud auth configure-docker
-# Prepare docker args.
+gcloud auth configure-docker asia-northeast1-docker.pkg.dev
+
 docker run  --rm --volume="$(pwd):/repo/" --workdir="/repo/" \
     "asia-northeast1-docker.pkg.dev/pfn-artifactregistry/public-ci-pfio/pfio:latest" \
     bash .pfnci/test-old.sh
