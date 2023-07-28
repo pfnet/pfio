ff8b0f67426116d13b8ca63ae86422fdb604eabb
Kota Uenishi <kota@preferred.jp>
2023-07-28 21:31:40 +0900

    wip

--- pfio/v2/gcs.py
+++ pfio/v2/gcs.py
@@ -1,24 +1,46 @@
+import base64
+import json
+
 from google.cloud import storage
 from google.cloud.storage.fileio import BlobReader, BlobWriter
 from google.oauth2 import service_account
 
 from .fs import FS, FileStat
 
+
 class ObjectStat(FileStat):
     def __init__(self, blob):
-        self.blob = blob
+        self.path = blob.path
+        self.size = blob.size
+        self.metadata = blob.metadata
+        self.crc32c = blob.crc32c
+        self.md5_hash = base64.b64decode(blob.md5_hash).hex()
 
 
 class GoogleCloudStorage(FS):
-    def __init__(self, bucket, prefix=None, key_path=None):
-        with open(key_path) as kp:
-            service_account_info = json.load(kp)
-            credentials = service_account.Credentials.from_service_account_info(service_account_info)
+    def __init__(self, bucket: str, prefix=None, key_path=None):
+        self.bucket_name = bucket
+        self.prefix = prefix
+        self.key_path = key_path
+        self._reset()
+
+    def _reset(self):
+        if self.key_path is None:
+            self.client = storage.Client()
+        else:
+            with open(self.key_path) as kp:
+                service_account_info = json.load(kp)
+                credentials = service_account.Credentials.from_service_account_info(service_account_info)
             self.client = storage.Client(credentials=credentials,
                                          project=credentials.project_id)
-            self.bucket = self.client.get_bucket(bucket)
-            self.bucket_name = bucket
-            self.prefix = prefix
+
+        # Caveat: You'll need ``roles/storage.insightsCollectorService`` role for the accessor instead.
+        # This is because ``roles/storage.objectViewer`` does not have ``storage.buckets.get``
+        # which is needed to call ``get_bucket()``.
+        #
+        # See also: https://cloud.google.com/storage/docs/access-control/iam-roles
+        self.bucket = self.client.get_bucket(self.bucket_name)
+        self.bucket_name = self.bucket
             
     def open(self, path, mode='r', **kwargs):
         blob = self.bucket.blob(os.path.join(self.prefix, path))
@@ -40,7 +62,7 @@ class GoogleCloudStorage(FS):
                 yield blob.path
 
     def stat(self, path):
-        return ObjectStat(self.bucket.blob(path))
+        return ObjectStat(self.bucket.get_blob(path))
 
     def isdir(self, path):
         return False
