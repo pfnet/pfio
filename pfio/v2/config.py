84b994bc65276ac3affda5dd144351766ff559f7
niboshi <niboshi000@gmail.com>
2023-07-31 03:17:11 +0000

    Add function interface to add custom scheme

---
+++ pfio/v2/config.py
@@ -0,0 +1,46 @@
+import configparser
+import os
+from typing import Dict, Optional
+
+
+def _default_config_file():
+    path = os.getenv('PFIO_CONFIG_PATH')
+    if path:
+        return path
+
+    basedir = os.getenv('XDG_CONFIG_HOME')
+    if not basedir:
+        basedir = os.path.join(os.getenv('HOME'), ".config")
+
+    return os.path.join(basedir, "pfio.ini")
+
+
+def _reload_config():
+    global _config
+    config = configparser.ConfigParser()
+    configfile = _default_config_file()
+    config.read(configfile)
+    _config = config
+
+
+def add_custom_scheme(name: str, scheme: str, data: Optional[Dict[str, str]] = None):
+    if data is None:
+        data = {}
+    else:
+        data = data.copy()
+
+    data["scheme"] = scheme
+    _config[name] = data
+
+
+def get_custom_scheme(name: str) -> Optional[Dict[str, str]]:
+    try:
+        return dict(_config[name])
+    except KeyError:
+        return None
+
+
+_config = None
+
+
+_reload_config()
