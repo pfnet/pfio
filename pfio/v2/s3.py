e18a8adaccf4afc7ba588737c10ad3a65bece05e
Alexis Vallet <alexis.vallet@gmail.com>
2023-10-06 14:37:34 +0900

    Fixed whitespace

--- pfio/v2/s3.py
+++ pfio/v2/s3.py
@@ -202,7 +202,7 @@ class _ObjectWriter:
                 hashlib.md5(data.encode()).digest()
             ).decode()
         num = len(self.parts) + 1
-        
+
         res = c.upload_part(Body=data, Bucket=b, Key=k,
                             PartNumber=num,
                             UploadId=self.mpu_id,
