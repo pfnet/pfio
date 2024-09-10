3e8411e62a713c74b274e92ceac065257d4320f6
funera1 <funeral7776@gmail.com>
2024-09-10 17:13:06 +0900

    Delete gcs_list_folders

--- pfio/v2/gcs.py
+++ pfio/v2/gcs.py
@@ -437,30 +437,3 @@ class GoogleCloudStorage(FS):
 
         return f"gs://{self.hostname}/{self.bucket}/{norm_path}"
     
-
-# def gcs_list_folders(bucket, prefix="", delimiter="/", guess_lexicographically_last_item="~", gcs_client=None):
-#     folders = set()
-#     prefix_parts = prefix.split(delimiter)
-#     start_offset = "/".join(prefix_parts)
-#     last_blob_name = None
-#     while True:
-#         blobs = list(gcs_client.list_blobs(
-#             bucket_or_name=bucket,
-#             prefix=prefix,
-#             start_offset=start_offset,
-#             max_results=1
-#         ))
-#         if not blobs:
-#             break
-#         blob = blobs[0]
-#         if last_blob_name == blob.name:
-#             raise Exception("Saw blob {} twice, try setting a different guess_lexicographically_last_item={}.".format(
-#                 repr(blob.name), repr(guess_lexicographically_last_item)
-#             ))
-#         folder = delimiter.join(blob.name.split(delimiter)[0:len(prefix_parts)] + [""])
-#         folders.add(folder)
-#         start_offset = folder + guess_lexicographically_last_item
-#         last_blob_name = blob.name
-#         try_characters = 1
-
-#     return folders
