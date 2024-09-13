4b10b1ec59320fc0643b62e883569c89abc3bd4f
Kiyoshi Mizumaru <kmizumar@preferred.jp>
2024-09-13 16:54:00 +0900

    fs.py: fix discrepancy in argname and docstring

--- pfio/v2/fs.py
+++ pfio/v2/fs.py
@@ -255,7 +255,7 @@ class FS(abc.ABC):
     def exists(self, path: str) -> bool:
         """Returns the existence of the path
 
-        When the ``file_path`` points to a symlink, the return value
+        When the ``path`` points to a symlink, the return value
         depends on the actual file instead of the link itself.
 
         """
@@ -278,7 +278,7 @@ class FS(abc.ABC):
         raise NotImplementedError()
 
     @abstractmethod
-    def remove(self, file_path: str, recursive: bool = False) -> None:
+    def remove(self, path: str, recursive: bool = False) -> None:
         """Removes a file or directory
 
            Args:
