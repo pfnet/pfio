bcc1d0ed782e9de042f034a3e800c214e3ace2c4
funera1 <funeral7776@gmail.com>
2024-09-18 15:01:40 +0900

    Fix PrefixStat and do not output own folder

--- pfio/v2/gcs.py
+++ pfio/v2/gcs.py
@@ -357,6 +357,7 @@ class GoogleCloudStorage(FS):
                 continue
 
             if detail:
+                print("_format_path(blob.name, path): ", _format_path(blob.name, path))
                 yield ObjectStat(blob, _format_path(blob.name, path))
             else:
                 yield _format_path(blob.name, path)
